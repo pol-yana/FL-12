7f9a28e44c689363e712c0ec5256d26f1c0f2c86
pol-yana <yana.polchyn@gmail.com>
2019-12-22 23:04:48 +0200

    add file with task1

---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,8 @@
+// Your code goes here
+var a = prompt("a=", a);
+var b = prompt("b=", b);
+var c = prompt("c=", c);
+
+if ((typeof(a)||typeof(b)||typeof(c)) != Number){
+    alert("Invalid input data");
+}
