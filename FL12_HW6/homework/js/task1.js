7b7961475928ffc0b6c96a0264852fc9bed19af9
pol-yana <yana.polchyn@gmail.com>
2019-12-22 23:53:28 +0200

    fix error after eslint

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,22 +1,24 @@
 // Your code goes here
-var a = parseInt(prompt("a=", a));
-var b = parseInt(prompt("b=", b));
-var c = parseInt(prompt("c=", c));
-
-if (isNaN(a)|| isNaN (b)|| isNaN (c)) {
-  alert("Invalid input data");
-}else{
-var d = b * b - 4 * a * c;
-alert("D=" + d);
+var a = parseInt(prompt('a=', a));
+var b = parseInt(prompt('b=', b));
+var c = parseInt(prompt('c=', c));
+let d;
+const two=2;
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert('Invalid input data');
+} else {
+  const n = 4;
+  d = b * b - n * a * c;
+  alert('D=' + d);
 }
 if (d > 0) {
-  var x1 = (-b + Math.sqrt(d)) / (2 * a);
-  var x2 = (-b - Math.sqrt(d)) / (2 * a);
-  alert("x1 = " + x1);
-  alert("x2 = " + x2);
-}else if(d == 0) {
-  var x = -b / (2 * a);
-  alert("x = " + x);
-}else{
-  alert ("No solution");
+  let x1 = (-b + Math.sqrt(d)) / (two * a);
+  let x2 = (-b - Math.sqrt(d)) / (two * a);
+  alert('x1 = ' + x1);
+  alert('x2 = ' + x2);
+} else if (d === 0) {
+  let x = -b / (two * a);
+  alert('x = ' + x);
+} else {
+  alert('No solution');
 }
