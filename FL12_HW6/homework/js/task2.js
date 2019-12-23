6d0142a908aaf679fcea4bf2277fe00d3dc3f9cb
pol-yana <yana.polchyn@gmail.com>
2019-12-23 23:24:46 +0200

    task2 after eslint

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,20 +1,22 @@
 // Your code goes here
-var a = prompt("a=", a);
-var b = prompt("b=", b);
-var c = prompt("c=", c);
+var a = parseInt(prompt('a=', a));
+var b = parseInt(prompt('b=', b));
+var c = parseInt(prompt('c=', c));
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-  alert("Input values should be ONLY numbers ");
-}
-if (a <= 0 || b <= 0 || c <= 0) {
-  alert("A triangle must have 3 sides with a positive definite length ");
-}
-if (a + b <= c || a + c <= b || b + c <= a) {
-  console.log("Triangle doesn’t exist");
-} else if ((a === b) === c) {
-  console.log("Equilateral triangle");
-} else if (a === b || a === c || b === c) {
-  console.log("Isosceles triangle");
-} else if (a != b || a != c || b != c) {
-  console.log("Scalene triangle");
+  alert('Input values should be ONLY numbers ');
+} else {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    alert('A triangle must have 3 sides with a positive definite length ');
+  } else {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      console.log('Triangle doesn’t exist');
+    } else if (a === b && a === c) {
+      console.log('Equilateral triangle');
+    } else if (a === b || a === c || b === c) {
+      console.log('Isosceles triangle');
+    } else if (a !== b && a !== c && b !== c) {
+      console.log('Scalene triangle');
+    }
+  }
 }
