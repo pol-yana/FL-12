9647d03193774145da03994bfd0e49bfa4a0a43e
pol-yana <yana.polchyn@gmail.com>
2019-12-23 23:59:08 +0200

    change window message

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -3,22 +3,21 @@ var a = parseInt(prompt('a=', a));
 var b = parseInt(prompt('b=', b));
 var c = parseInt(prompt('c=', c));
 let d;
-const two=2;
+const two = 2;
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-  alert('Invalid input data');
+  console.log('Invalid input data');
 } else {
   const n = 4;
   d = b * b - n * a * c;
-  alert('D=' + d);
 }
 if (d > 0) {
   let x1 = (-b + Math.sqrt(d)) / (two * a);
   let x2 = (-b - Math.sqrt(d)) / (two * a);
-  alert('x1 = ' + x1);
-  alert('x2 = ' + x2);
+  console.log('x1 = ' + x1);
+  console.log('x2 = ' + x2);
 } else if (d === 0) {
   let x = -b / (two * a);
-  alert('x = ' + x);
+  console.log('x = ' + x);
 } else {
-  alert('No solution');
+  console.log('No solution');
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -10,7 +10,7 @@ if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('A triangle must have 3 sides with a positive definite length ');
   } else {
     if (a + b <= c || a + c <= b || b + c <= a) {
-      console.log('Triangle doesn’t exist');
+      alert('Triangle doesn’t exist');
     } else if (a === b && a === c) {
       console.log('Equilateral triangle');
     } else if (a === b || a === c || b === c) {
