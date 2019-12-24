7e13ebaed9d2a8f5bacd420382d288740be78e26
pol-yana <yana.polchyn@gmail.com>
2019-12-24 18:03:47 +0200

    round nambers in task1

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,19 +4,18 @@ var b = parseInt(prompt('b=', b));
 var c = parseInt(prompt('c=', c));
 let d;
 const two = 2;
-if (isNaN(a) || isNaN(b) || isNaN(c)) {
+if (isNaN(a) || isNaN(b) || isNaN(c) ||a===0){
   console.log('Invalid input data');
 } else {
   const n = 4;
   d = b * b - n * a * c;
 }
 if (d > 0) {
-  let x1 = (-b + Math.sqrt(d)) / (two * a);
-  let x2 = (-b - Math.sqrt(d)) / (two * a);
-  console.log('x1 = ' + x1);
-  console.log('x2 = ' + x2);
+  let x1 = Math.round((-b + Math.sqrt(d)) / (two * a));
+  let x2 = Math.round((-b - Math.sqrt(d)) / (two * a));
+  console.log('x1 = ' + x1 + ' and x2 = '+ x2);
 } else if (d === 0) {
-  let x = -b / (two * a);
+  let x =Math.round( -b / (two * a));
   console.log('x = ' + x);
 } else {
   console.log('No solution');
