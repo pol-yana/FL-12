72cff25dfe92f02078ca3f2a89b134ba472bae8b
pol-yana <yana.polchyn@gmail.com>
2019-12-29 01:18:23 +0200

    task2 after eslint

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,66 +1,70 @@
 // Your code goes here
 let n = 1;
 let totalPrize = 0;
-let game = confirm("Do you want to play a game?");
+let game = confirm('Do you want to play a game?');
 if (!game) {
-  alert("You did not become a billionaire, but can.");
+  alert('You did not become a billionaire, but can.');
 } else {
   while (game) {
     let prize = 100 * Math.pow(2, n - 1);
     let randomNumber =
       Math.floor(Math.random() * (Math.floor(4 + 4 * n) - Math.ceil(0) + 1)) +
       Math.ceil(0);
+      
+      console.log(randomNumber);
     let attempt1 = prompt(
-      "Choose a roulette pocket number from 0 to " +
+      'Choose a roulette pocket number from 0 to ' +
         (4 + 4 * n) +
-        "\nAttempts left 3\nTotal prize: " +
+        '\nAttempts left 3\nTotal prize: ' +
         totalPrize +
-        " $\nPosible prize on current attempt " +
+        ' $\nPosible prize on current attempt ' +
         prize +
-        "$"
+        '$'
     );
-
-    if (attempt1 === randomNumber) {
-      alert("Congratulation, you won!   Your prize is: " + prize + "$.");
-      game = confirm("Do you want to continue?");
+    if (parseInt(attempt1) === randomNumber) {
+      alert('Congratulation, you won!   Your prize is: ' + prize + '$.');
+      game = confirm('Do you want to continue?');
       totalPrize = totalPrize + prize;
+      
     } else {
       let prize = 50 * Math.pow(2, n - 1);
       let attempt2 = prompt(
-        "Choose a roulette pocket number from 0 to " +
+        'Choose a roulette pocket number from 0 to ' +
           (4 + 4 * n) +
-          "\nAttempts left 3\nTotal prize: " +
+          '\nAttempts left 2\nTotal prize: ' +
           totalPrize +
-          " $\nPosible prize on current attempt " +
+          ' $\nPosible prize on current attempt ' +
           prize +
-          "$"
+          '$'
       );
-      if (attempt2 === randomNumber) {
-        alert("Congratulation, you won!   Your prize is: " + prize + "$.");
-        game = confirm("Do you want to continue?");
+      if (parseInt(attempt2) === randomNumber) {
+        alert('Congratulation, you won!   Your prize is: ' + prize + '$.');
+        game = confirm('Do you want to continue?');
         totalPrize = totalPrize + prize;
+        
       } else {
         let prize = 25 * Math.pow(2, n - 1);
         let attempt3 = prompt(
-          "Choose a roulette pocket number from 0 to " +
+          'Choose a roulette pocket number from 0 to ' +
             (4 + 4 * n) +
-            "\nAttempts left 3\nTotal prize: " +
+            '\nAttempts left 1\nTotal prize: ' +
             totalPrize +
-            " $\nPosible prize on current attempt " +
+            ' $\nPosible prize on current attempt ' +
             prize +
-            "$"
+            '$'
         );
-        if (attempt3 === randomNumber) {
-          alert("Congratulation, you won!   Your prize is: " + prize + "$.");
-          game = confirm("Do you want to continue?");
+        if (parseInt(attempt3) === randomNumber) {
+          alert('Congratulation, you won!   Your prize is: ' + prize + '$.');
+          game = confirm('Do you want to continue?');
           totalPrize = totalPrize + prize;
         } else {
           alert(
-            "Thank you for your participation. Your prize is: " +
+            'Thank you for your participation. Your prize is: ' +
               totalPrize +
-              " $"
+              ' $'
           );
-          game = confirm("Do you want to continue?");
+          n=0;
+          game = confirm('Do you want to continue?');
         }
       }
     }
