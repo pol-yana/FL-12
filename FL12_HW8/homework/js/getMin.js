a170328f0966ac25d412cf651b89ff5793868eac
pol-yana <yana.polchyn@gmail.com>
2019-12-29 19:23:14 +0200

    add task getMin

---
+++ FL12_HW8/homework/js/getMin.js
@@ -0,0 +1,10 @@
+function getMin(numbers) {
+  let min = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  return min;
+}
+getMin([]);
