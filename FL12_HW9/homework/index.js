6f9fd296bb3d00906d0172fbf1d88717192eb759
pol-yana <yana.polchyn@gmail.com>
2020-01-13 16:25:00 +0200

    add task fifth

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,17 +1,17 @@
 // Your code goes here
 //task1
 function convert(...args) {
-    let arr = [];
-    for (var i = 0; i < args.length; i++) {
-      if (typeof(args[i]) === 'string') {
-        arr.push(parseInt(args[i]));
-      } else {
-        arr.push(args[i].toString());
-      }
+  let arr = [];
+  for (var i = 0; i < args.length; i++) {
+    if (typeof args[i] === "string") {
+      arr.push(parseInt(args[i]));
+    } else {
+      arr.push(args[i].toString());
     }
-    return arr;
   }
-  convert("1", 2, 3, "4");
+  return arr;
+}
+convert("1", 2, 3, "4");
 
 //task2
 function executeforEach(arr, callback) {
@@ -23,16 +23,33 @@ executeforEach([1, 2, 3], function(el) {
 
 //task3
 function mapArray(arr, callback) {
-    let result = [];
-    for (var i = 0; i < arr.length; i++) {
-      if (typeof arr[i] === "string") {
-        result.push(callback(Number(arr[i])));
-      }else{
-          result.push(callback(arr[i]))
-      }
+  let result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      result.push(callback(Number(arr[i])));
+    } else {
+      result.push(callback(arr[i]));
     }
-    return result;
   }
-  mapArray([2, "5", 8], function(el) {
-    return el + 3;
-  });
+  return result;
+}
+mapArray([2, "5", 8], function(el) {
+  return el + 3;
+});
+
+//task4
+function filterArray(arr, callback) {
+  arr.forEach(el => callback(el));
+}
+filterArray([2, 5, 8], function(el) {
+  return el % 2 === 0;
+});
+
+//task5
+function flipOver(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+flipOver("hey world");
