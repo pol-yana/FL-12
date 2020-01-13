188e19db3a32f50d634ea8a0e4faacfb2716eecb
pol-yana <yana.polchyn@gmail.com>
2020-01-13 14:10:47 +0200

    change tasks first, secont and third

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,20 +1,38 @@
 // Your code goes here
 //task1
-function convert(args) {
-  let arr = [];
-  for (let i = 0; i<args.length; i++) {
-    console.log(args[i]);
-    if (typeof args[i] === 'string') {
-      arr.push(parseInt(args[i]));
-    } else {
-      arr.push(args[i].toString());
+function convert(...args) {
+    let arr = [];
+    for (var i = 0; i < args.length; i++) {
+      if (typeof(args[i]) === 'string') {
+        arr.push(parseInt(args[i]));
+      } else {
+        arr.push(args[i].toString());
+      }
     }
+    return arr;
   }
-  return arr;
-}
-convert(6, '8', 3, '9');
+  convert("1", 2, 3, "4");
 
 //task2
-function multiply(arr) {
-  arr.forEach(element => console.log(element * 2));
+function executeforEach(arr, callback) {
+  arr.forEach(el => callback(el));
 }
+executeforEach([1, 2, 3], function(el) {
+  console.log(el * 2);
+});
+
+//task3
+function mapArray(arr, callback) {
+    let result = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === "string") {
+        result.push(callback(Number(arr[i])));
+      }else{
+          result.push(callback(arr[i]))
+      }
+    }
+    return result;
+  }
+  mapArray([2, "5", 8], function(el) {
+    return el + 3;
+  });
