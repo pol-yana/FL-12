5e0db434d03ffb374349218c6504029afdb975d7
pol-yana <yana.polchyn@gmail.com>
2020-01-17 01:18:28 +0200

    add second part of metods

--- FL12_HW10/homework/fighters_game.js
+++ FL12_HW10/homework/fighters_game.js
@@ -1,29 +1,50 @@
 /* Your code goes here */
 function Fighter(obj) {
-    this.name = obj.name;
-    this.damage = obj.damage;
-    this.hp = obj.hp;
-    this.strength = obj.strength;
-    this.agility = obj.agility;
+  this.name = obj.name;
+  this.damage = obj.damage;
+  this.hp = obj.hp;
+  this.strength = obj.strength;
+  this.agility = obj.agility;
+  this.win=0;
+  this.loss=0;
 
-    this.getName = function(){
-        return 'Name : ' + this.name;
-    }
+  this.getName = function() {
+    return "Name : " + this.name;
+  };
 
-    this.getDamage =function(){
-        return 'Damage: ' + this.damage;
-    }
+  this.getDamage = function() {
+    return "Damage: " + this.damage;
+  };
 
-    this.getStrength =function(){
-        return 'Strength: ' + this.strength;
-    }
+  this.getStrength = function() {
+    return "Strength: " + this.strength;
+  };
 
-    this.getAgility =function(){
-        return 'Agility: ' + this.agility;
-    }
+  this.getAgility = function() {
+    return "Agility: " + this.agility;
+  };
 
-    this.getHealth =function(){
-        return 'Health: ' + this.hp;
-    }
+  this.getHealth = function() {
+    return "Health: " + this.hp;
+  };
 
+  this.logCombatHistory = function() {
+    return "Name:" + this.name + ", Wins:" + this.win + ", Losses:" + this.loss;
+  };
+
+  this.health = function(n) {
+    this.hp + n <= 100 ? (this.hp = this.hp + n) : (this.hp = 100);
+  };
+
+  this.dealDamage = function(n) {
+    this.hp - n > 0 ? (this.hp = this.hp - n) : (this.hp = 0);
+  };
+
+  this.addWin = function() {
+    this.win++;
+  };
+
+  this.addLoss = function() {
+    this.loss++;
+  };
 }
