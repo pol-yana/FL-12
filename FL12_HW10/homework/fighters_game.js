48fcd8d0ac01d6f492bb90aa4c24edc9f712de64
pol-yana <yana.polchyn@gmail.com>
2020-01-17 00:39:04 +0200

    add metods

--- FL12_HW10/homework/fighters_game.js
+++ FL12_HW10/homework/fighters_game.js
@@ -1,1 +1,29 @@
 /* Your code goes here */
+function Fighter(obj) {
+    this.name = obj.name;
+    this.damage = obj.damage;
+    this.hp = obj.hp;
+    this.strength = obj.strength;
+    this.agility = obj.agility;
+
+    this.getName = function(){
+        return 'Name : ' + this.name;
+    }
+
+    this.getDamage =function(){
+        return 'Damage: ' + this.damage;
+    }
+
+    this.getStrength =function(){
+        return 'Strength: ' + this.strength;
+    }
+
+    this.getAgility =function(){
+        return 'Agility: ' + this.agility;
+    }
+
+    this.getHealth =function(){
+        return 'Health: ' + this.hp;
+    }
+
+}
