6652d8f69dadf16b476aa24f6b193f48378e136c
pol-yana <yana.polchyn@gmail.com>
2020-01-26 20:27:39 +0200

    add function

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -1,21 +1,21 @@
 const structure = [
   {
     folder: true,
-    title: "Films",
+    title: 'Films',
     children: [
       {
-        title: "Iron Man.avi"
+        title: 'Iron Man.avi'
       },
       {
         folder: true,
-        title: "Fantasy",
+        title: 'Fantasy',
         children: [
           {
-            title: "The Lord of the Rings.avi"
+            title: 'The Lord of the Rings.avi'
           },
           {
             folder: true,
-            title: "New folder 1",
+            title: 'New folder 1',
             children: false
           }
         ]
@@ -24,29 +24,29 @@ const structure = [
   },
   {
     folder: true,
-    title: "Documents",
+    title: 'Documents',
     children: [
       {
         folder: true,
-        title: "EPAM Homework answers",
+        title: 'EPAM Homework answers',
         children: null
       }
     ]
   }
 ];
 
-const rootNode = document.getElementById("root");
+const rootNode = document.getElementById('root');
 
 function build(elem) {
+  let node = document.createElement('LI');
+  let textnode = document.createTextNode(elem.title);
+  let nestedNode = document.createElement('UL');
+
   if (elem.folder) {
     if (elem.children !== false && elem.children !== null) {
-      var node = document.createElement("LI");
-      node.classList.add("caret");
 
-      var nestedNode = document.createElement("UL");
-      nestedNode.classList.add("nested");
-
-      var textnode = document.createTextNode(elem.title);
+      node.classList.add('caret');
+      nestedNode.classList.add('nested');
       node.appendChild(textnode);
 
       if (elem.children) {
@@ -56,27 +56,23 @@ function build(elem) {
       }
 
       node.appendChild(nestedNode);
+
     } else {
-      var node = document.createElement("LI");
-      node.classList.add("caret");
-
-      var textnode = document.createTextNode(elem.title);
+      
+      node.classList.add('caret');
       node.appendChild(textnode);
+      nestedNode.classList.add('nested');
 
-      var nestedNode = document.createElement("UL");
-      nestedNode.classList.add("nested");
-
-      var nestedTextNode = document.createTextNode("Folder is empty");
+      let nestedTextNode = document.createTextNode('Folder is empty');
       nestedNode.appendChild(nestedTextNode);
 
       node.appendChild(nestedNode);
     }
   } else {
-    var node = document.createElement("LI");
-    node.classList.add("file");
     
-    var textnode = document.createTextNode(elem.title);
+    node.classList.add('file');
     node.appendChild(textnode);
+    
   }
 
   return node;
@@ -86,15 +82,13 @@ for (let i = 0; i < structure.length; i++) {
   rootNode.appendChild(build(structure[i]));
 }
 
-var toggler = document.getElementsByClassName("caret");
-for (let i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
-  });
-}
+var root = document.getElementById('root');
 
-// var icon = document.createElement("I");
-// icon = classList.add('material-icons');
-// rootNode.appendChild(icon);
-//var iconText = document.createTextNode('folder')
+root.onclick = function(event){
+  event.target.querySelector('.nested').classList.toggle('active');
+  event.target.classList.toggle('caret-down');
+}
+ 
+
+
+
