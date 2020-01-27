f5233c30986c5c66aeb23c8ee58e769e5679a5a5
pol-yana <yana.polchyn@gmail.com>
2020-01-28 00:14:39 +0200

    add list of keys

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,78 +1,105 @@
-
-
 // Your code goes here
 
 var root = document.getElementById("root");
 console.log(root);
-var btmAddNew = document.createElement("button");
-btmAddNew.innerHTML='Add new';
-root.appendChild(btmAddNew);
+
+var btnAddNew = document.createElement("button");
+btnAddNew.innerHTML = "Add new";
+root.appendChild(btnAddNew);
 console.log(root);
 
-btmAddNew.addEventListener('click', function(){
-    var createForm = document.createElement('form');
-createForm.setAttribute('action', "");
-createForm.setAttribute('metod', 'post');
-root.appendChild(createForm);
-
-var keyLabel = document.createElement('label');
-keyLabel.innerHTML = "Key:";
-createForm.appendChild(keyLabel);
-
-
-var keyInput = document.createElement('input');
-keyInput.setAttribute('type', "text");
-keyInput.setAttribute('name', 'dkey');
-createForm.appendChild(keyInput);
-
-var linebreak = document.createElement('br');
-createForm.appendChild(linebreak);
-
-var valueLabel = document.createElement('label');
-valueLabel.innerHTML = "Value:";
-createForm.appendChild(valueLabel);
-
-
-var valueInput = document.createElement('input');
-valueInput.setAttribute('type', "text");
-valueInput.setAttribute('name', 'dvalue');
-createForm.appendChild(valueInput);
-
-var linebreak = document.createElement('br');
-createForm.appendChild(linebreak);
-
-var submit = document.createElement('input');
-submit.setAttribute('type', 'submit');
-submit.setAttribute('name', 'dsubmit');
-submit.setAttribute('value', "submit");
-createForm.appendChild(submit);
-    
+btnAddNew.addEventListener("click", function() {
+  if( !document.getElementById("id-form") ){
+    var createForm = document.createElement("div");
+    // createForm.setAttribute("action", "");
+    // createForm.setAttribute("metod", "post");
+    createForm.setAttribute("id", "id-form");
+    root.appendChild(createForm);
+  
+    var keyLabel = document.createElement("label");
+    keyLabel.innerHTML = "Key:";
+    createForm.appendChild(keyLabel);
+  
+    var keyInput = document.createElement("input");
+    keyInput.setAttribute("type", "text");
+    keyInput.setAttribute("name", "dkey");
+    createForm.appendChild(keyInput);
+  
+    var linebreak1 = document.createElement("br");
+    createForm.appendChild(linebreak1);
+  
+    var valueLabel = document.createElement("label");
+    valueLabel.innerHTML = "Value:";
+    createForm.appendChild(valueLabel);
+  
+    var valueInput = document.createElement("input");
+    console.log(valueInput);
+    valueInput.value = '';
+    valueInput.setAttribute("type", "text");
+    valueInput.setAttribute("name", "dvalue");
+    createForm.appendChild(valueInput);
+  
+    var lineBreak2 = document.createElement("br");
+    createForm.appendChild(lineBreak2);
+  
+    var submit = document.createElement("button");
+  
+  submit.innerHTML = "SAVE";
+    // submit.setAttribute("type", "submit");
+    // submit.setAttribute("name", "dsubmit");
+    // submit.setAttribute("value", "submit");
+    createForm.appendChild(submit);
+  
+    submit.onclick = function() {
+      // console.log(keyInput.value, valueInput.value);
+      window.location.hash = '#/add'; 
+      addToLocalStorage(keyInput.value, valueInput.value);
+      getAllKeyValueFromLocalStorage();
+    };
+  } else{
+    var inputs = document.getElementsByTagName('input');
+    for(let i=0; i<inputs.length; i++){
+      inputs[i].value = '';
+    }
+  }
+  
 });
-// var createForm = document.createElement('form');
-// createForm.setAttribute('action', "");
-// createForm.setAttribute('metod', 'post');
-// root.appendChild(createForm);
 
-// var keyLabel = document.createElement('label');
-// keyLabel.innerHTML = "Key:";
-// createForm.appendChild(keyLabel);
+// todo: button EDIT
+function buttonEdit() {
+  var btnEdit = document.createElement("button");
+  btnAddNew.innerHTML = "Edit";
+  root.appendChild(btnEdit);
+}
+// todo: button DELETE
+function buttonDelete() {
+  var btnDelete = document.createElement("button");
+  btnAddNew.innerHTML = "Delete";
+  root.appendChild(btnDelete);
+}
+// todo: add to localStorage
+function addToLocalStorage(key, value) {
+  // console.log(key, " ==== ", value);
+  localStorage.setItem(key, value);
+}
+// todo: edit in localStorage
 
+// todo: remove from localStorage
 
-// var keyInput = document.createElement('input');
-// keyInput.setAttribute('type', "text");
-// keyInput.setAttribute('name', 'dname');
-// createForm.appendChild(keyInput);
+function getAllKeyValueFromLocalStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let value = localStorage.getItem(key);
+    // console.log(key, " == ", value);
+    showKeyValueInHtml(key, value);
+  }
+}
+function showKeyValueInHtml(key, value) {
+  let showKey = document.createElement("p");
+  showKey.innerHTML = key+' = ' + value;
+  root.appendChild(showKey);
 
-// var linebreak = document.createElement('br');
-// createForm.appendChild(linebreak);
-
-// var valueLabel = document.createElement('label');
-// valueLabel.innerHTML = "Value:";
-// createForm.appendChild(valueLabel);
-
-
-// var valueInput = document.createElement('input');
-// valueInput.setAttribute('type', "text");
-// valueInput.setAttribute('name', 'dname');
-// createForm.appendChild(valueInput);
-
+  // let showValue = document.createElement("p");
+  // showValue.innerHTML = value;
+  // root.appendChild(showValue);
+}
